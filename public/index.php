00f98295027ef9fd6b01957f31475a7e13eeb105
kincaidandrew <kincaidandrew2000@outlook.com>
2024-10-30 14:38:16 +0000

    Stage 2 Completed - Update and Delete

--- public/index.php
+++ public/index.php
@@ -6,6 +6,12 @@
         <li>
             <a href="read.php"><strong>Read</strong></a> - find a user
         </li>
+        <li>
+            <a href="update.php"><strong>Update</strong></a> - edit a user
+        </li>
+        <li>
+            <a href="delete.php"><strong>Delete</strong></a> - delete a user
+        </li>
     </ul>
     <?php include "templates/footer.php";?>
    
